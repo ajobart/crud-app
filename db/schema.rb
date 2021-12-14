e9046b7c39ea331b11091ba829696ea5bf27f028
Alexis Jobart <jobart.pro@gmail.com>
2021-12-14 10:48:23 +0100

    create model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_084438) do
+ActiveRecord::Schema.define(version: 2021_12_14_091537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "model"
+    t.integer "year"
+    t.string "status"
+    t.string "color"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "warehouse_id"
+    t.index ["user_id"], name: "index_cars_on_user_id"
+    t.index ["warehouse_id"], name: "index_cars_on_warehouse_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +36,18 @@ ActiveRecord::Schema.define(version: 2021_12_14_084438) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "warehouses", force: :cascade do |t|
+    t.string "address"
+    t.string "capacity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "cars", "users"
+  add_foreign_key "cars", "warehouses"
 end
